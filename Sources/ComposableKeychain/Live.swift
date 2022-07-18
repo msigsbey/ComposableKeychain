385c807536895935a8c60c042248a0b822bd1b20
Michael Sigsbey <msigsbey@gmail.com>
2022-07-17 21:48:00 -0500

    Add dispatch queue

--- Sources/ComposableKeychain/Live.swift
+++ Sources/ComposableKeychain/Live.swift
@@ -5,45 +5,57 @@
 //  Created by Michael Sigsbey on 7/17/22.
 //
 
+import Foundation
 import KeychainAccess
 import ComposableArchitecture
 
 extension KeychainClient {
     public static func live(
-        keychain: Keychain
+        keychain: Keychain,
+        queue: AnySchedulerOf<DispatchQueue>
     ) -> Self {
         Self(
             getString: { key in
                 Effect
                     .catching { try keychain.getString(key.rawValue) }
+                    .subscribe(on: queue)
+                    .fireAndForget()
             },
             getData: { key in
                 Effect
                     .catching { try keychain.getData(key.rawValue) }
+                    .subscribe(on: queue)
+                    .fireAndForget()
             },
             setString: { string, key in
                 Effect
                     .catching { try keychain.set(string, key: key.rawValue) }
+                    .subscribe(on: queue)
                     .fireAndForget()
             },
             setData: { data, key in
                 Effect
                     .catching { try keychain.set(data, key: key.rawValue) }
+                    .subscribe(on: queue)
                     .fireAndForget()
             },
             remove: { key in
                 Effect
                     .catching { try keychain.remove(key.rawValue) }
+                    .subscribe(on: queue)
                     .fireAndForget()
             },
             removeAll: {
                 Effect
                     .catching { try keychain.removeAll() }
+                    .subscribe(on: queue)
                     .fireAndForget()
             },
             contains: { key in
                 Effect
                     .catching { try keychain.contains(key.rawValue) }
+                    .subscribe(on: queue)
+                    .fireAndForget()
             }
         )
     }
